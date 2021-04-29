e3d5a3f7e9b7c70f1df1d1f1b465c9346f9e76db
anitah <anitahkimanzi96@gmail.com>
2021-04-29 14:21:34 -0400

    changed registration readmore button

--- src/components/data/content.js
+++ src/components/data/content.js
@@ -296,7 +296,7 @@ export const data = {
         title : "Registration",
         image : register,
         link : "/register",
-        linkText  : "Join",
+        linkText  : "Register",
         readmore : "Read more",
         moreInfo : [
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
