518c539ddc576ed5b631f1f3624f4ae8349b04df
anitah <anitahkimanzi96@gmail.com>
2021-04-29 09:10:40 -0400

    added not found component

--- src/components/notfound/error.js
+++ src/components/notfound/error.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function error() {
     return (
-        <div>
-            
+        <div className="notfound row no-gutters h-100">
+            <div className="col-12 my-auto text-center">
+                <h1>404</h1>
+                <p>Page not found</p>
+            </div>
         </div>
     )
 }
